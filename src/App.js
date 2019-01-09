e837ff5dfcf680ef824b98711e2694c876d44feb
Nikita Zamiatin <4406015@gmail.com>
2019-01-09 14:36:59 -0500

    first state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      message : "Welcome to new Todo in 2019"
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>
+           {this.state.message}
+          </h2>
         </header>
       </div>
     );
